1778bee1b205e98b01168f9ab3c551f39e1c6c81
Shaan Chanchani <shaanchanchani@Shaans-MacBook-Pro.local>
2023-01-12 19:44:22 -0500

    Updated model to predict percentiles

--- draftPredictionModel.py
+++ draftPredictionModel.py
@@ -5,19 +5,35 @@ from sklearn.ensemble import RandomForestRegressor
 
 def main():
     data = pd.read_csv('NFLdraftdata.csv')
-    
     qb_features = ['pos','pick','player', 'year', 'height_inches', 'weight', 'passing.attempts', 'passing.comp.pct',
        'passing.completions', 'passing.games', 'passing.pass.ints',
        'passing.pass.tds', 'passing.pass.yards', 'passing.seasons']
     df = data[qb_features]
+    df = df[df['pos'] == 'QB']
+    df = df.dropna()
+    
+    df['PowRank'] = None
+    
+    years = pd.unique(df['year'])
+    
+    for year in years:
+      percentsArr = []
+      newdf = df[df['year'] == year]
+      newdf['Rank'] = newdf['pick'].rank(method='min',ascending=True)
+      for i in newdf.index:
+         if newdf['pick'][i] != 300:
+            perc = ((len(newdf)-(newdf['Rank'][i]))/len(newdf)) * 100
+         else:
+            perc = 0
+         percentsArr.append(perc)
+      df.loc[newdf.index,'PowRank'] = percentsArr
+      
+    df = df.replace(',','', regex=True)
+    prediction_target = df.PowRank.astype(float)
+
     qb_features_clean = ['year','height_inches', 'weight', 'passing.attempts', 'passing.comp.pct',
        'passing.completions', 'passing.games', 'passing.pass.ints',
        'passing.pass.tds', 'passing.pass.yards', 'passing.seasons']
-
-    df = df[df['pos'] == 'QB']
-    df = df.dropna()
-    df = df.replace(',','', regex=True)
-    prediction_target = df.pick.astype(float)
     df = df[qb_features_clean]
     df = df.astype(float)
 
